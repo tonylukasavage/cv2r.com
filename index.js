bcb2886782980d223e175864ca0b4de3507da24f
Tony Lukasavage <anthony.lukasavage@gmail.com>
2019-08-11 01:31:53 -0400

    more stuff working

--- index.js
+++ index.js
@@ -12,6 +12,74 @@ function randomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+const crew = [
+	{
+		name: 'BloodSweatAndCode',
+		role: 'Creator',
+		social: [
+			{ name: 'twitch', 'link': 'https://www.twitch.tv/bloodsweatandcode' },
+			{ name: 'speedrun.com', 'link': 'https://www.speedrun.com/user/BloodSweatAndCode' },
+			{ name: 'SRL', 'link': 'http://www.speedrunslive.com/profiles/#!/BloodSweatAndCode/1' },
+			{ name: 'youtube', 'link': 'https://www.youtube.com/channel/UCaqBNgyY6bL7yZwSSiIHRkQ' },
+			{ name: 'twitter', 'link': 'https://twitter.com/tonylukasavage' }
+		]
+	},
+	{
+		name: 'Burb',
+		role: 'Contributor, Tester',
+		social: [
+			{ name: 'twitch', 'link': 'https://www.twitch.tv/burb__' },
+			{ name: 'speedrun.com', 'link': 'https://www.speedrun.com/user/Burb' },
+			{ name: 'SRL', 'link': 'http://www.speedrunslive.com/profiles/#!/Burb/1' },
+			{ name: 'youtube', 'link': 'https://www.youtube.com/user/burbruee' },
+			{ name: 'twitter', 'link': 'https://www.twitter.com/burbruee' }
+		]
+	},
+	{
+		name: 'Sathdresh',
+		role: 'Tester',
+		social: [
+			{ name: 'twitch', 'link': 'https://www.twitch.tv/sathdresh' },
+			{ name: 'speedrun.com', 'link': 'https://www.speedrun.com/user/Sathdresh' },
+			{ name: 'SRL', 'link': 'http://www.speedrunslive.com/profiles/#!/sathdresh/1' },
+			{ name: 'youtube', 'link': 'https://www.youtube.com/channel/UCz9a0Yk4-omxav26_CIIeQw' },
+			{ name: 'twitter', 'link': 'https://www.twitter.com/sathdresh' }
+		]
+	},
+	{
+		name: '2snek',
+		role: 'Tester',
+		social: [
+			{ name: 'twitch', 'link': 'https://www.twitch.tv/2snek' },
+			{ name: 'speedrun.com', 'link': 'https://www.speedrun.com/user/2_snek' },
+			{ name: 'youtube', 'link': 'https://www.youtube.com/user/2+snek' },
+			{ name: 'twitter', 'link': 'https://www.twitter.com/2_snek' }
+		]
+	},
+	{
+		name: 'Retrogaming2084',
+		role: 'Tester',
+		social: [
+			{ name: 'twitch', 'link': 'https://www.twitch.tv/Retrogaming2084' },
+			{ name: 'speedrun.com', 'link': 'https://www.speedrun.com/user/Retrogaming2084' },
+			{ name: 'SRL', 'link': 'http://www.speedrunslive.com/profiles/#!/Retrogaming2084' },
+			{ name: 'twitter', 'link': 'https://www.twitter.com/Retrogaming2084' }
+		]
+	},
+	{
+		name: 'drcossack',
+		role: 'Tester',
+		social: [
+			{ name: 'twitch', 'link': 'https://www.twitch.tv/drcossack' },
+			{ name: 'speedrun.com', 'link': 'https://www.speedrun.com/user/drcossack' },
+			{ name: 'SRL', 'link': 'http://www.speedrunslive.com/profiles/#!/drcossack/1' }
+		]
+	}
+
+
+];
+crew.forEach(c => c.image = `/img/people/${c.name}.png`);
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -20,7 +88,7 @@ express()
 	.get('/start', (req, res) => res.render('pages/start'))
 	.get('/dev', (req, res) => res.render('pages/dev'))
 	.get('/doc', (req, res) => res.render('pages/doc'))
-	.get('/credits', (req, res) => res.render('pages/credits'))
+	.get('/credits', (req, res) => res.render('pages/credits', { crew }))
   .post('/patch', (req, res) => {
   	exec('./node_modules/.bin/cv2-rando --json', function(err, stdout, stderr) {
   		if (err) {

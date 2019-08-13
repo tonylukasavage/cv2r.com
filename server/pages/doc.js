59c61cfa710675a8a6911b5efda991358546aebe
Tony Lukasavage <anthony.lukasavage@gmail.com>
2019-08-13 16:27:45 -0400

    mostly done with items

--- server/pages/doc.js
+++ server/pages/doc.js
@@ -1,5 +1,27 @@
 const core = require('cv2-rando/lib/core');
 
+const items = [
+  { name: 'whips', count: 4, notes: 'Whips are upgraded progressively. This means that any whip you obtain will always give you the next most powerful whip. For example, if you have the leather whip, the next whip check you obtain will give you the thorn whip.' },
+  { name: 'crystals', count: 3, notes: 'Just as whips, crystals are upgraded progressively. The first crystal you obtain will always be white, the next blue, and then finally red.' },
+  { name: 'dagger', count: 1, notes: '' },
+  { name: 'silver knife', count: 1, notes: '' },
+  { name: 'golden knife', count: 1, notes: '' },
+  { name: 'holy water', count: 1, notes: '' },
+  { name: 'diamond', count: 1, notes: '' },
+  { name: 'sacred flame', count: 1, notes: '' },
+  { name: 'oak stake', count: 5, notes: '' },
+  { name: 'rib', count: 1, notes: '' },
+  { name: 'heart', count: 1, notes: '' },
+  { name: 'eyeball', count: 1, notes: '' },
+  { name: 'nail', count: 1, notes: '' },
+  { name: 'ring', count: 1, notes: '' },
+  { name: 'silk bag', count: 1, notes: '' },
+  { name: 'magic cross', count: 1, notes: '' },
+  { name: 'laurels', count: 5, notes: '' },
+  { name: 'garlic', count: 2, notes: '' }
+];
+items.forEach(i => i.image = `/img/sprites/${i.name.replace(/\s+/g, '-')}.png`);
+
 const reqOrder = [
   'holy water',
   'white crystal',
@@ -50,5 +72,8 @@ module.exports = function(app) {
       checks.push({ actor: name, location, image, requirements });
     });
   });
-  app.get('/doc', (req, res) => res.render('pages/doc', { checks }));
+
+
+
+  app.get('/doc', (req, res) => res.render('pages/doc', { checks, items }));
 };

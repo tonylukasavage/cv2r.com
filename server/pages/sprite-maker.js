c92f2e1c4447fb11c76d4cfbcb6e0306ee2aaaa7
BloodSweatAndCode <anthony.lukasavage@gmail.com>
2020-01-30 15:22:57 -0500

    move sprite maker data to server

--- server/pages/sprite-maker.js
+++ server/pages/sprite-maker.js
@@ -1,7 +1,8 @@
 const { colors } = require('../NES.js');
+const sprites = require('../sprites.js');
 
 module.exports = function(app) {
   app.get('/sprite-maker', (req, res) => {
-  	res.render('pages/sprite-maker', { colors });
+  	res.render('pages/sprite-maker', { colors, sprites });
   });
 };

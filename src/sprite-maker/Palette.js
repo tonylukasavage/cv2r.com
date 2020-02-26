f384bc94939fe3194feb826c940eb9bbaa7b1d12
Tony Lukasavage <alukasavage@innovu.com>
2020-02-26 17:22:12 -0500

    palette indexes, restructure

--- src/sprite-maker/Palette.js
+++ src/sprite-maker/Palette.js
@@ -1,15 +1,23 @@
 const { palette } = require('./data');
 
+const template = `
+<div class="palette-button">
+	<div class="palette-button-corner">
+		<div class="palette-button-index">1</div>
+	</div>
+</div>
+`;
+
 class Palette {
 	constructor() {
 		palette.forEach((p, index) => {
-			const button = $('<button></button>');
-			button.addClass('palette-button');
+			const button = $(template);
 			if (index === 0) {
 				button.addClass('palette-button-selected');
 			}
 			button.css('background-color', '#' + p.hex);
 			button.data('pi', index);
+			button.find('.palette-button-index').text(index === 0 ? 'X' : index);
 			$('#palette-container').append(button);
 
 			button.click(function() {

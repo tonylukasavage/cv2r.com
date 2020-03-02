f12a49e2b5541f1a1971c37ae12ca835d5069501
BloodSweatAndCode <anthony.lukasavage@gmail.com>
2020-03-01 19:38:59 -0500

    move export into SpriteMaker.js

--- src/sprite-maker/SpriteMaker.js
+++ src/sprite-maker/SpriteMaker.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+const data = require('./data');
 const ColorPicker = require('./ColorPicker');
 const Editor = require('./Editor');
 const Palette = require('./Palette');
@@ -34,7 +36,43 @@ class SpriteMaker {
 		});
 
 		$('#sprite-patch').click(function() {
-			tiles.export();
+			const chrPixelLength = 64;
+			const spritePatches = [];
+
+			tiles.pixels.forEach(pixels => {
+				const bytes = [];
+				const { offset } = pixels;
+				let pixelCount = 0;
+
+				pixels.layout.forEach(layout => {
+					const pixelOffset = layout * 16;
+					let byte1 = '', byte2 = '';
+					let byteIndex = 0;
+
+					for (let i = 0; i < chrPixelLength; i++) {
+						const { paletteIndex } = pixels[pixelCount++];
+						byte1 += paletteIndex % 2;
+						byte2 += paletteIndex > 1 ? 1 : 0;
+						if (i % 8 === 7) {
+							bytes[byteIndex + pixelOffset] = parseInt(byte1, 2);
+							bytes[byteIndex + pixelOffset + 8] = parseInt(byte2, 2);
+							byteIndex++;
+							if (byteIndex % 8 === 0) {
+								byteIndex += 8;
+							}
+							byte1 = '';
+							byte2 = '';
+						}
+					}
+				});
+				spritePatches.push({ offset, bytes });
+			});
+
+			const patch = _.template(patchTemplate)({
+				spritePatches: JSON.stringify(spritePatches, null, 2),
+				palette: data.palette.slice(1).map(p => p.index).join(',')
+			});
+			console.log(patch);
 		});
 	}
 
@@ -46,3 +84,30 @@ class SpriteMaker {
 }
 
 window.SpriteMaker = SpriteMaker;
+
+const patchTemplate = `const spritePatches = <%= spritePatches %>;
+
+const offsets = [ 0, 0x2000, 0x4000, 0x6000, 0x8000, 0x9000, 0xB000, 0x17000 ];
+const finalSpritePatch = [];
+for (let i = 0; i < offsets.length; i++) {
+	spritePatches.forEach(sp => {
+		finalSpritePatch.push({
+			offset: sp.offset + offsets[i],
+			bytes: sp.bytes.slice(0)
+		});
+	});
+}
+
+// palette
+finalSpritePatch.push({
+	offset: 117439,
+	bytes: [<%= palette %>]
+});
+
+module.exports = {
+	id: 'test-sprite',
+	name: 'Test Sprite',
+	description: 'cv2r.com Sprite Maker test sprite',
+	patch: finalSpritePatch
+};
+`;

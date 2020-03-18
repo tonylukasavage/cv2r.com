ef5ddc2a867d5d81fab65b488914ce1cccc69148
Tony Lukasavage <alukasavage@innovu.com>
2020-03-18 11:56:14 -0400

    Create anon-pull-request.sh

---
+++ tools/anon-pull-request.sh
@@ -0,0 +1,15 @@
+curl -X POST \
+  https://xrbhog4g8g.execute-api.eu-west-2.amazonaws.com/prod/prb0t \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -d '{
+  "user": "BloodSweatAndCode",
+  "repo": "cv2r",
+  "description": "test description",
+  "title": "test title",
+  "commit": "test commit",
+  "files": [
+  	{"path": "patch/simon/simon/index.json", "content": "index.json"},
+    {"path": "patch/simon/simon/simon.png", "content": "image content"}
+  ]
+}'
